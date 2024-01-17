815acd83b8a6e04b657e98dd04076ea3cd37b728
Nayaka <97321138+NayakaZNA@users.noreply.github.com>
2024-01-18 03:51:24 +0700

    Edit HTML and JS.

--- script.js
+++ script.js
@@ -1,67 +1,77 @@
 var mlg = {
-    city: "Malang",
+    name: "Malang",
     lat: -7.9666,
     long: 112.6326,
-    routes: ["AL", "MM"],
+    routes: {
+      AL: "https://angkoot-rndn.web.app/malang_kt_al.kml",
+      MM: "https://angkoot-rndn.web.app/malang_kt_mm.kml"  
+    },
 };
 
 var smd = {
-    city: "Sumedang",
+    name: "Sumedang",
     lat: -6.8381,
     long: 107.9275,
-    routes: ["03", "04"],
+    routes: {
+      N04: "https://angkoot-rndn.web.app/sumedang_kb_04.kml",
+    },
 };
 
 var bdg = {
-    city: "Bandung",
+    name: "Bandung",
     lat: -6.9390,
     long: 107.6692,
-    routes: ["43"],
+    routes: {
+      N43: "https://angkoot-rndn.web.app/bandung_kbt_43.kml",
+  }
+  ,
 };
 
 var bwi = {
-  city: "Banyuwangi",
-  lat: -6.9390,
-  long: 107.6692,
-  routes: ["9", "11"],
+  name: "Banyuwangi",
+  lat: -8.2192,
+  long: 114.3692,
+  routes: {
+    LIN7: "https://angkoot-rndn.web.app/banyuwangi_kb_7.kml",
+    LIN9: "https://angkoot-rndn.web.app/banyuwangi_kb_9.kml"
+  },
 };
 
-cities = [mlg, smd, bdg, bwi]
+var cities = [mlg, smd, bdg, bwi]
 
-function myMap(key) {
-    latitude = key.lat;
-    longitude = key.long;
-    var mapProp= {
-      center:new google.maps.LatLng(latitude, longitude),
-      zoom:13,
-      mapTypeId: 'roadmap',
-      mapTypeControl: false,
-      overviewMapControl: false,
-      streetViewControl: false,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    const Smd04 = new google.maps.KmlLayer({
-        url: "https://angkoot-rndn.web.app/sumedang_kb_04.kml",
-        map: map,
-      });
-    const BdgKBt43 = new google.maps.KmlLayer({
-      url: "https://angkoot-rndn.web.app/bandung_kbt_43.kml",
-      map: map,
-    });
-    const MlgAL = new google.maps.KmlLayer({
-        url: "https://angkoot-rndn.web.app/malang_kt_al.kml",
-        map: map,
-      });
-    const MlgMM = new google.maps.KmlLayer({
-      url: "https://angkoot-rndn.web.app/malang_kt_mm.kml",
-      map: map,
-    });
-    const Bwi07 = new google.maps.KmlLayer({
-      url: "https://angkoot-rndn.web.app/banyuwangi_kb_7.kml",
-      map: map,
-    });
-    const Bwi09 = new google.maps.KmlLayer({
-      url: "https://angkoot-rndn.web.app/banyuwangi_kb_9.kml",
-      map: map,
-    });
-    }
+
+function getSelectedCity() {
+  // Mengambil nilai dari pilihan dropdown oleh pengguna
+  var cityDropdown = document.getElementById('pilihKota');
+  var selectedCityName = cityDropdown.value;
+  // Mencari object yang bersesuaian dengan kota input
+  var selectedCity = cities.find(city => city.name == selectedCityName);
+  document.getElementById('routeDisplay').innerHTML = 'Selected City: ' + selectedCity.lat;
+  myMap(selectedCity)
+  return selectedCity;
+}
+
+function myMap(city) {
+  // Mengambil data kota
+  latitude  = city.lat;
+  longitude = city.long;
+
+  // Konfigurasi peta
+  var mapProp= {
+    center:new google.maps.LatLng(latitude, longitude),
+    zoom:13,
+    mapTypeId: 'roadmap',
+    mapTypeControl: false,
+    overviewMapControl: false,
+    streetViewControl: false,
+  };
+
+  // Menampilkan peta pada HTML
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+  // Menampilkan layer KML
+  var kmlLayer = new google.maps.KmlLayer({
+    
+  }
+  );
+}
